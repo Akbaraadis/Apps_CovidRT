9683e98d985b2298ffa34084e864c54d136d5068
akbaraadis <akbaradis99@gmail.com>
2020-08-31 19:09:54 +0700

    Login user autentication

--- app/src/main/java/com/project/apps_covidrt/LoginActivity.kt
+++ app/src/main/java/com/project/apps_covidrt/LoginActivity.kt
@@ -6,20 +6,33 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.project.apps_covidrt.rt.menuutama.MainActivity
 import com.project.apps_covidrt.rt.pendaftaran.PendaftaranRTActivity
 import com.project.apps_covidrt.warga.menuutama.MainWargaActivity
 import com.project.apps_covidrt.warga.pendaftaran.PendaftaranWargaActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_pendaftaran_rt.*
 import kotlinx.android.synthetic.main.popup_login_daftar.*
+import org.json.JSONObject
+import java.util.HashMap
 
 class LoginActivity : AppCompatActivity() {
+
+    var status_user = "2"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -56,15 +69,16 @@ class LoginActivity : AppCompatActivity() {
 
         btn_login.setOnClickListener {
             val check = spinner.selectedItemPosition
-            if (check == 0) {
-                val wargamain = Intent(this, MainWargaActivity::class.java).also {
-                    startActivity(it)
-                }
+            if(TextUtils.isEmpty(et_login_username.getText().toString()) || TextUtils.isEmpty(et_login_password.getText().toString())){
+                Toast.makeText(applicationContext, "Email atau Password belum diisi", Toast.LENGTH_SHORT).show()
             }
-            else{
-                val rtmain = Intent(this, MainActivity::class.java).also {
-                    startActivity(it)
+            else {
+                if (check == 0) {
+                    status_user = "2"
+                } else {
+                    status_user = "1"
                 }
+                jsonParse()
             }
         }
 
@@ -104,6 +118,49 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+    }
+
+    fun jsonParse() {
+
+
+        //url
+        val url:String = "http://test-api.online/covid-rt/public/login"
+
+        //RequestQueue initialized
+        val mRequestQueue = Volley.newRequestQueue(this)
+
+        //String Request initialized
+        val mStringRequest = object : StringRequest(Request.Method.POST, url, Response.Listener { response ->
+
+            if(status_user=="2"){
+                val gotomainwarga = Intent(this, MainWargaActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            else{
+                val gotopengelolart = Intent(this, MainActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }, Response.ErrorListener { error ->
+            Log.i("This is the error", "Error :" + error.toString())
+            Toast.makeText(applicationContext, "Email belum terdaftar", Toast.LENGTH_SHORT).show()
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val params2 = HashMap<String, String>()
+                params2["username"] = et_login_username.text.toString()
+                params2["password"] = et_login_password.text.toString()
+                params2["status_user"] = status_user.toString()
+                return JSONObject(params2 as Map<*, *>).toString().toByteArray()
+            }
+
+        }
+        mRequestQueue!!.add(mStringRequest!!)
     }
 
 }

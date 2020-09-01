5af93fded5d0408b2893e29bee205cfa184fd214
akbaraadis <akbaradis99@gmail.com>
2020-09-01 14:47:54 +0700

    Fitur ganti password dan logout done

--- app/src/main/java/com/project/apps_covidrt/warga/gantipassword/GantiPasswordWargaActivity.kt
+++ app/src/main/java/com/project/apps_covidrt/warga/gantipassword/GantiPasswordWargaActivity.kt
@@ -1,12 +1,80 @@
 package com.project.apps_covidrt.warga.gantipassword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.project.apps_covidrt.LoginActivity
 import com.project.apps_covidrt.R
+import com.project.apps_covidrt.warga.menuutama.MainWargaActivity
+import kotlinx.android.synthetic.main.activity_ganti_password_warga.*
+import kotlinx.android.synthetic.main.activity_pendaftaran_warga.*
+import org.json.JSONObject
+import java.util.HashMap
 
 class GantiPasswordWargaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ganti_password_warga)
+
+        btn_ganti_password.setOnClickListener(View.OnClickListener {
+            jsonParse()
+        })
     }
+
+    fun jsonParse() {
+
+        //Token
+        val intent = intent
+        var tokennya= intent.getStringExtra("Token")
+
+        //url
+        val url:String = "http://test-api.online/covid-rt/public/change-password"
+
+        //RequestQueue initialized
+        val mRequestQueue = Volley.newRequestQueue(this)
+
+        //String Request initialized
+        val mStringRequest = object : StringRequest(Request.Method.POST, url, Response.Listener { response ->
+            Toast.makeText(applicationContext, "Ubah Password Berhasil", Toast.LENGTH_SHORT).show()
+            var gotomainwarga = Intent(this, MainWargaActivity::class.java)
+            gotomainwarga.putExtra("Token", tokennya)
+            startActivity(gotomainwarga)
+
+
+        }, Response.ErrorListener { error ->
+            Log.i("This is the error", "Error :" + error.toString())
+            Toast.makeText(applicationContext, "Gagal, Password Tidak Cocok", Toast.LENGTH_SHORT).show()
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val params2 = HashMap<String, String>()
+                params2["old_password"] = et_ganti_password_lama.text.toString().trim()
+                params2["new_password"] = et_ganti_password_baru.text.toString().trim()
+                params2["new_password_confirmation"] = et_ganti_password_konfirm.text.toString().trim()
+                return JSONObject(params2 as Map<*, *>).toString().toByteArray()
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["token"] = tokennya.toString()
+                return headers
+            }
+
+        }
+        mRequestQueue!!.add(mStringRequest!!)
+    }
+
 }
--- app/src/main/java/com/project/apps_covidrt/warga/menuutama/MainWargaActivity.kt
+++ app/src/main/java/com/project/apps_covidrt/warga/menuutama/MainWargaActivity.kt
@@ -6,8 +6,15 @@ import android.content.Intent
 import android.os.Build.ID
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.project.apps_covidrt.LoginActivity
 import com.project.apps_covidrt.R
 import com.project.apps_covidrt.warga.editprofile.EditProfileWargaActivity
@@ -15,6 +22,7 @@ import com.project.apps_covidrt.warga.gantipassword.GantiPasswordWargaActivity
 import com.project.apps_covidrt.warga.kondisikesehatan.KondisiKesehatanActivity
 import com.project.apps_covidrt.warga.kondisikesejahteraan.KondisiKesejahteraanActivity
 import com.project.apps_covidrt.warga.pendaftaran.PendaftaranWargaActivity
+import kotlinx.android.synthetic.main.activity_edit_profile_warga.*
 import kotlinx.android.synthetic.main.activity_main_warga.*
 import kotlinx.android.synthetic.main.activity_pendaftaran_warga.*
 import kotlinx.android.synthetic.main.popup_editprofile_warga.*
@@ -46,9 +54,7 @@ class MainWargaActivity : AppCompatActivity() {
             startActivity(inputkesejahteraan)
         })
         cl_warga_logout.setOnClickListener(View.OnClickListener {
-            val logouta = Intent(this, LoginActivity::class.java).also {
-                startActivity(it)
-            }
+            jsonParseGet()
         })
 
     }
@@ -76,5 +82,43 @@ class MainWargaActivity : AppCompatActivity() {
             poppass.putExtra("Token", tokennya1)
             startActivity(poppass)
         }
+    }
+
+    fun jsonParseGet() {
+
+        val intent = intent
+        var tokennya= intent.getStringExtra("Token")
+
+        //url
+        val url:String = "http://test-api.online/covid-rt/public/logout"
+
+        //RequestQueue initialized
+        val mRequestQueue = Volley.newRequestQueue(this)
+
+        //String Request initialized
+        val mStringRequest = object : JsonObjectRequest(Request.Method.GET, url,null, Response.Listener { response ->
+            Toast.makeText(applicationContext, "Anda Telah Keluar", Toast.LENGTH_SHORT).show()
+            val logout = Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+
+        }, Response.ErrorListener { error ->
+            Log.i("This is the error", "Error :" + error.toString())
+            Toast.makeText(applicationContext, "Data Gagal Ditampilkan", Toast.LENGTH_SHORT).show()
+        })
+        {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["token"] = tokennya.toString()
+                return headers
+            }
+
+        }
+        mRequestQueue!!.add(mStringRequest!!)
     }
 }

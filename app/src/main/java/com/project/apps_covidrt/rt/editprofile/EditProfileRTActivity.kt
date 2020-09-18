f5cec595d7d12a93de22ff6e402f2508c800b50c
akbaraadis <akbaradis99@gmail.com>
2020-09-18 11:06:08 +0700

    nambahin spinner di edit profile

--- app/src/main/java/com/project/apps_covidrt/rt/editprofile/EditProfileRTActivity.kt
+++ app/src/main/java/com/project/apps_covidrt/rt/editprofile/EditProfileRTActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
@@ -18,22 +21,54 @@ import com.project.apps_covidrt.rt.menuutama.MainActivity
 import com.project.apps_covidrt.warga.menuutama.MainWargaActivity
 import kotlinx.android.synthetic.main.activity_edit_profile_r_t.*
 import kotlinx.android.synthetic.main.activity_edit_profile_warga.*
+import kotlinx.android.synthetic.main.activity_pendaftaran_warga.*
 import org.json.JSONObject
 import java.util.HashMap
 
 class EditProfileRTActivity : AppCompatActivity() {
+
+    var check:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile_r_t)
 
         jsonParseGet()
 
+        val status = resources.getStringArray(R.array.Kecamatan)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.sp_editrt_kecamatan)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, status
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    check = spinner.selectedItemPosition
+                    checkkelurahan()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
         btn_editrt_simpan.setOnClickListener(View.OnClickListener {
-            if(TextUtils.isEmpty(et_editrt_email.getText().toString()) ||
+            if(et_editrt_rt.length() != 3 || et_editrt_rw.length() != 3){
+                Toast.makeText(this,"Gunakan 3 digit pada keterangan RT dan RW", Toast.LENGTH_SHORT).show()
+            }
+            else if(TextUtils.isEmpty(et_editrt_email.getText().toString()) ||
                 TextUtils.isEmpty(et_editrt_nama.getText().toString()) ||
                 TextUtils.isEmpty(et_editrt_nik.getText().toString()) ||
-                TextUtils.isEmpty(et_editrt_kecamatan.getText().toString()) ||
-                TextUtils.isEmpty(et_editrt_kelurahan.getText().toString()) ||
                 TextUtils.isEmpty(et_editrt_rt.getText().toString()) ||
                 TextUtils.isEmpty(et_editrt_rw.getText().toString())){
 
@@ -43,6 +78,48 @@ class EditProfileRTActivity : AppCompatActivity() {
                 jsonParse()
             }
         })
+    }
+
+    fun checkkelurahan(){
+        if (check == 0) {
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Ciledug)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_editrt_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
+        else if (check == 1) {
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Karang)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_editrt_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
+        else{
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Larangan)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_editrt_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
     }
 
     fun jsonParseGet() {
@@ -61,8 +138,82 @@ class EditProfileRTActivity : AppCompatActivity() {
             et_editrt_email.setText(response.getString("username"))
             et_editrt_nama.setText(response.getString("nama"))
             et_editrt_nik.setText(response.getString("nik"))
-            et_editrt_kecamatan.setText(response.getString("kecamatan"))
-            et_editrt_kelurahan.setText(response.getString("kelurahan"))
+
+            val kecamatanedit = response.getString("kecamatan")
+            val kelurahanedit = response.getString("kelurahan")
+            if(kecamatanedit == "Ciledug"){
+                sp_editrt_kecamatan.setSelection(0)
+                sp_editrt_kelurahan.resources.getStringArray(R.array.Kecamatan_Ciledug)
+
+                if (kelurahanedit == "Paninggilan") {
+                    sp_editrt_kelurahan.setSelection(0)
+                } else if (kelurahanedit == "Paninggilan Utara") {
+                    sp_editrt_kelurahan.setSelection(1)
+                } else if (kelurahanedit == "Parung Serab") {
+                    sp_editrt_kelurahan.setSelection(2)
+                } else if (kelurahanedit == "Sudimara Barat") {
+                    sp_editrt_kelurahan.setSelection(3)
+                } else if (kelurahanedit == "Sudimara Jaya") {
+                    sp_editrt_kelurahan.setSelection(4)
+                } else if (kelurahanedit == "Sudimara Selatan") {
+                    sp_editrt_kelurahan.setSelection(5)
+                } else if (kelurahanedit == "Sudimara Timur") {
+                    sp_editrt_kelurahan.setSelection(6)
+                } else {
+                    sp_editrt_kelurahan.setSelection(7)
+                }
+            }
+
+            else if(kecamatanedit == "Karang Tengah"){
+                sp_editrt_kecamatan.setSelection(1)
+                sp_editrt_kelurahan.resources.getStringArray(R.array.Kecamatan_Karang)
+
+                if (kelurahanedit == "Karang Mulya") {
+                    sp_editrt_kelurahan.setSelection(0)
+                } else if (kelurahanedit == "Karang Tengah") {
+                    sp_editrt_kelurahan.setSelection(1)
+                } else if (kelurahanedit == "Karang Timur") {
+                    sp_editrt_kelurahan.setSelection(2)
+                } else if (kelurahanedit == "Parung Jaya") {
+                    sp_editrt_kelurahan.setSelection(3)
+                } else if (kelurahanedit == "Pedurenan") {
+                    sp_editrt_kelurahan.setSelection(4)
+                } else if (kelurahanedit == "Pondok Bahar") {
+                    sp_editrt_kelurahan.setSelection(5)
+                } else {
+                    sp_editrt_kelurahan.setSelection(6)
+                }
+            }
+            else{
+                sp_editrt_kecamatan.setSelection(2)
+                sp_editrt_kelurahan.resources.getStringArray(R.array.Kecamatan_Larangan)
+
+                if(kelurahanedit == "Cipadu"){
+                    sp_editrt_kelurahan.setSelection(0)
+                }
+                else if(kelurahanedit == "Cipadu Jaya"){
+                    sp_editrt_kelurahan.setSelection(1)
+                }
+                else if(kelurahanedit == "Gaga"){
+                    sp_editrt_kelurahan.setSelection(2)
+                }
+                else if(kelurahanedit == "Kreo"){
+                    sp_editrt_kelurahan.setSelection(3)
+                }
+                else if(kelurahanedit == "Kreo Selatan"){
+                    sp_editrt_kelurahan.setSelection(4)
+                }
+                else if(kelurahanedit == "Larangan Indah"){
+                    sp_editrt_kelurahan.setSelection(5)
+                }
+                else if(kelurahanedit == "Larangan Selatan"){
+                    sp_editrt_kelurahan.setSelection(6)
+                }
+                else{
+                    sp_editrt_kelurahan.setSelection(7)
+                }
+            }
+
             et_editrt_rt.setText(response.getString("rt"))
             et_editrt_rw.setText(response.getString("rw"))
 
@@ -120,8 +271,8 @@ class EditProfileRTActivity : AppCompatActivity() {
                 params2["username"] = et_editrt_email.text.toString().trim()
                 params2["nama"] = et_editrt_nama.text.toString().trim()
                 params2["nik"] = et_editrt_nik.text.toString().trim()
-                params2["kecamatan"] = et_editrt_kecamatan.text.toString().trim()
-                params2["kelurahan"] = et_editrt_kelurahan.text.toString().trim()
+                params2["kecamatan"] = sp_editrt_kecamatan.selectedItem.toString().trim()
+                params2["kelurahan"] = sp_editrt_kelurahan.selectedItem.toString().trim()
                 params2["rt"] = et_editrt_rt.text.toString().trim()
                 params2["rw"] = et_editrt_rw.text.toString().trim()
                 return JSONObject(params2 as Map<*, *>).toString().toByteArray()
--- app/src/main/java/com/project/apps_covidrt/warga/editprofile/EditProfileWargaActivity.kt
+++ app/src/main/java/com/project/apps_covidrt/warga/editprofile/EditProfileWargaActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
@@ -40,6 +43,7 @@ class EditProfileWargaActivity : AppCompatActivity() {
     var flag_hipertensi = "0"
     var flag_perokok = "0"
 
+    var check:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +58,32 @@ class EditProfileWargaActivity : AppCompatActivity() {
         }
 
         jsonParseGet()
+
+        val status = resources.getStringArray(R.array.Kecamatan)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.sp_edit_kecamatan)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, status
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    check = spinner.selectedItemPosition
+                    checkkelurahan()
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
 
         rb_lk2.setOnClickListener{
             ll_hamil.visibility = View.GONE
@@ -113,10 +143,12 @@ class EditProfileWargaActivity : AppCompatActivity() {
             else
                 flag_diabet = "0"
 
-            if(TextUtils.isEmpty(et_edit_email.getText().toString()) ||
+            if(et_edit_rt.length() != 3 || et_edit_rw.length() != 3){
+                Toast.makeText(this,"Gunakan 3 digit pada keterangan RT dan RW", Toast.LENGTH_SHORT).show()
+            }
+            else if(TextUtils.isEmpty(et_edit_email.getText().toString()) ||
                 TextUtils.isEmpty(et_edit_nama.getText().toString()) || TextUtils.isEmpty(et_edit_nokk.getText().toString()) ||
                 TextUtils.isEmpty(et_edit_nik.getText().toString()) || TextUtils.isEmpty(et_edit_tanggallahir.getText().toString()) ||
-                TextUtils.isEmpty(et_edit_kecamatan.getText().toString()) || TextUtils.isEmpty(et_edit_kelurahan.getText().toString()) ||
                 TextUtils.isEmpty(et_edit_alamat.getText().toString()) || TextUtils.isEmpty(et_edit_rt.getText().toString()) ||
                 TextUtils.isEmpty(et_edit_rw.getText().toString()) || TextUtils.isEmpty(et_edit_nohp.getText().toString())){
 
@@ -126,6 +158,48 @@ class EditProfileWargaActivity : AppCompatActivity() {
                 jsonParse()
             }
         })
+    }
+
+    fun checkkelurahan(){
+        if (check == 0) {
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Ciledug)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_edit_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
+        else if (check == 1) {
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Karang)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_edit_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
+        else{
+            val status_lurah = resources.getStringArray(R.array.Kecamatan_Larangan)
+
+            // access the spinner
+            val spinner_lurah = findViewById<Spinner>(R.id.sp_edit_kelurahan)
+            if (spinner_lurah != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, status_lurah
+                )
+                spinner_lurah.adapter = adapter
+            }
+        }
     }
 
     private fun showDatePickerDialog() {
@@ -182,8 +256,8 @@ class EditProfileWargaActivity : AppCompatActivity() {
                 params2["username"] = et_edit_email.text.toString().trim()
                 params2["nama"] = et_edit_nama.text.toString().trim()
                 params2["nik"] = et_edit_nik.text.toString().trim()
-                params2["kecamatan"] = et_edit_kecamatan.text.toString().trim()
-                params2["kelurahan"] = et_edit_kelurahan.text.toString().trim()
+                params2["kecamatan"] = sp_edit_kecamatan.selectedItem.toString().trim()
+                params2["kelurahan"] = sp_edit_kelurahan.selectedItem.toString().trim()
                 params2["rt"] = et_edit_rt.text.toString().trim()
                 params2["rw"] = et_edit_rw.text.toString().trim()
                 params2["no_kk"] = et_edit_nokk.text.toString().trim()
@@ -231,8 +305,82 @@ class EditProfileWargaActivity : AppCompatActivity() {
             et_edit_email.setText(response.getString("username"))
             et_edit_nama.setText(response.getString("nama"))
             et_edit_nik.setText(response.getString("nik"))
-            et_edit_kecamatan.setText(response.getString("kecamatan"))
-            et_edit_kelurahan.setText(response.getString("kelurahan"))
+
+            val kecamatanedit = response.getString("kecamatan")
+            val kelurahanedit = response.getString("kelurahan")
+            if(kecamatanedit == "Ciledug"){
+                sp_edit_kecamatan.setSelection(0)
+                sp_edit_kelurahan.resources.getStringArray(R.array.Kecamatan_Ciledug)
+
+                    if (kelurahanedit == "Paninggilan") {
+                        sp_edit_kelurahan.setSelection(0)
+                    } else if (kelurahanedit == "Paninggilan Utara") {
+                        sp_edit_kelurahan.setSelection(1)
+                    } else if (kelurahanedit == "Parung Serab") {
+                        sp_edit_kelurahan.setSelection(2)
+                    } else if (kelurahanedit == "Sudimara Barat") {
+                        sp_edit_kelurahan.setSelection(3)
+                    } else if (kelurahanedit == "Sudimara Jaya") {
+                        sp_edit_kelurahan.setSelection(4)
+                    } else if (kelurahanedit == "Sudimara Selatan") {
+                        sp_edit_kelurahan.setSelection(5)
+                    } else if (kelurahanedit == "Sudimara Timur") {
+                        sp_edit_kelurahan.setSelection(6)
+                    } else {
+                        sp_edit_kelurahan.setSelection(7)
+                    }
+            }
+
+            else if(kecamatanedit == "Karang Tengah"){
+                sp_edit_kecamatan.setSelection(1)
+                sp_edit_kelurahan.resources.getStringArray(R.array.Kecamatan_Karang)
+
+                    if (kelurahanedit == "Karang Mulya") {
+                        sp_edit_kelurahan.setSelection(0)
+                    } else if (kelurahanedit == "Karang Tengah") {
+                        sp_edit_kelurahan.setSelection(1)
+                    } else if (kelurahanedit == "Karang Timur") {
+                        sp_edit_kelurahan.setSelection(2)
+                    } else if (kelurahanedit == "Parung Jaya") {
+                        sp_edit_kelurahan.setSelection(3)
+                    } else if (kelurahanedit == "Pedurenan") {
+                        sp_edit_kelurahan.setSelection(4)
+                    } else if (kelurahanedit == "Pondok Bahar") {
+                        sp_edit_kelurahan.setSelection(5)
+                    } else {
+                        sp_edit_kelurahan.setSelection(6)
+                    }
+            }
+            else{
+                sp_edit_kecamatan.setSelection(2)
+                sp_edit_kelurahan.resources.getStringArray(R.array.Kecamatan_Larangan)
+
+                    if(kelurahanedit == "Cipadu"){
+                        sp_edit_kelurahan.setSelection(0)
+                    }
+                    else if(kelurahanedit == "Cipadu Jaya"){
+                        sp_edit_kelurahan.setSelection(1)
+                    }
+                    else if(kelurahanedit == "Gaga"){
+                        sp_edit_kelurahan.setSelection(2)
+                    }
+                    else if(kelurahanedit == "Kreo"){
+                        sp_edit_kelurahan.setSelection(3)
+                    }
+                    else if(kelurahanedit == "Kreo Selatan"){
+                        sp_edit_kelurahan.setSelection(4)
+                    }
+                    else if(kelurahanedit == "Larangan Indah"){
+                        sp_edit_kelurahan.setSelection(5)
+                    }
+                    else if(kelurahanedit == "Larangan Selatan"){
+                        sp_edit_kelurahan.setSelection(6)
+                    }
+                    else{
+                        sp_edit_kelurahan.setSelection(7)
+                    }
+            }
+
             et_edit_rt.setText(response.getString("rt"))
             et_edit_rw.setText(response.getString("rw"))
             et_edit_nokk.setText(response.getJSONObject("warga").getString("no_kk"))
